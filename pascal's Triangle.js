5e0fae83dc0e280583235349c208d3d5feec9d99
mayuanying <286108514@qq.com>
2015-12-24 18:01:44 +0800

    pascal's triangle

---
+++ pascal's Triangle.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    var result = [];
+    if(numRows===0) return result;
+    else result = result.concat(generate(numRows-1));
+    var arr = [];
+    for(var m=1;m<=numRows;m++){
+        arr.unshift(getNum(numRows,m,result[result.length-1]));
+    }
+    result[numRows-1]=arr;
+    return result;
+};
+function getNum(n,m,prev){
+    if(m === 1) return 1;
+    else if(m === n) return 1;
+    else return prev[m-2]+prev[m-1];
+}
+// var generate = function(numRows) {
+//     var result = [];
+//     for(var i=1;i<=numRows;i++){
+//         var arr = [];
+//         for(var m=1;m<=i;m++){
+//             arr.unshift(getNum(i,m));
+//         }
+//         result[i-1]=arr;
+//     }
+//     return result;
+// };
+// function getNum(n,m){
+//     if(m === 1) return 1;
+//     else if(m === n) return 1;
+//     else return getNum(n-1,m-1)+getNum(n-1,m);
+// }
